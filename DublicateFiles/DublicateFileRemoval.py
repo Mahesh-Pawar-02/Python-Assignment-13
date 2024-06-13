e46ab82e60e20a9605e6c11c347b1f53830f3f7e
Mahesh Dinkar Pawar <maheshpawar30627@gmail.com>
2024-06-13 22:43:40 +0530

    Update 3.0.0 stable

--- DublicateFiles/DublicateFileRemoval.py
+++ DublicateFiles/DublicateFileRemoval.py
@@ -1,29 +1,34 @@
 import sys
 import time
 import schedule
-from duplicate_removal_module import *
+from Duplicate_Removal_Module import *
 
 def main():
+    print("------------------------------------------------------------------------")
+    print("---------------------Created By Mahesh Pawar----------------------------")
+    print("------------------------------------------------------------------------")
+    print("\nApplication name : "+sys.argv[0],"\n")
+    print("------------------------------------------------------------------------")
+
     if len(sys.argv) != 4:
-        print("Usage: python script.py <directory> <interval> <email>")
+        print("Usage: python DublicateFileRemoval.py <directory> <interval> <email>")
         sys.exit(1)
 
     directory = sys.argv[1]
     interval = sys.argv[2]
     email = sys.argv[3]
 
-
     try:
         validate_directory(directory)
         validate_interval(interval)
         validate_email(email)
-        print(email,"this email verified successfuly..")
-        print("wait for deleting dublicate file from",directory)
+        print(email,"this email verified successfully..\n")
+        print(f"this   {interval} minute for deleting dublicate file from",directory,"\n")
+        print("------------------------------------------------------------------------")
 
         interval_minutes = int(interval)
         
-        schedule.every(interval_minutes).minutes.do(main_task, directory, email)
-
+        schedule.every(interval_minutes).minutes.do(Main_Task, directory, email,interval)
         while True:
             schedule.run_pending()
             time.sleep(1)
--- DublicateFiles/duplicate_removal_module.py
+++ DublicateFiles/duplicate_removal_module.py
@@ -8,35 +8,18 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
-def calculate_checksum(file_path):
-    try:
-        sha256_hash = hashlib.sha256()
-        with open(file_path, "rb") as f:
-            for byte_block in iter(lambda: f.read(4096), b""):
-                sha256_hash.update(byte_block)
-        return sha256_hash.hexdigest()
-    except Exception as e:
-        print(f"Error calculating checksum for {file_path}: {str(e)}")
-        return None
+def hashfile(Path, blocksize = 1024):
+    fd = open(Path,"rb")
+    hasher = hashlib.md5()
+    buffer = fd.read(blocksize)
 
-def find_duplicates(directory):
-    try:
-        checksums = {}
-        duplicates = []
-        for root, _, files in os.walk(directory):
-            for file in files:
-                file_path = os.path.join(root, file)
-                checksum = calculate_checksum(file_path)
-                if checksum in checksums:
-                    duplicates.append(file_path)
-                else:
-                    checksums[checksum] = file_path
-        return duplicates
-    except Exception as e:
-        print(f"Error finding duplicates in {directory}: {str(e)}")
-        return []
+    while len(buffer) > 0:
+        hasher.update(buffer)
+        buffer = fd.read(blocksize)
+    fd.close()
+    return hasher.hexdigest()
 
-def delete_files(files):
+def Delete_Files(files):
     try:
         for file in files:
             os.remove(file)
@@ -44,30 +27,38 @@ def delete_files(files):
     except Exception as e:
         print(f"Error deleting file {file}: {str(e)}")
 
-def send_email(log_file, email_id, stats):
+def Mail_Sender(log_file, email_id, stats):
     try:
         msg = MIMEMultipart()
         msg['From'] = "try.web.new@gmail.com"
         msg['To'] = email_id
-        msg['Subject'] = "Duplicate File Removal Log"
+        msg['Subject'] = "Duplicate Files Removal Log"
 
         body = f"""\
-    Hello..!
+        <html>
+        <body style="font-family: Verdana, sans-serif; color: blue; font-size: large;">
+    <p>Hello..!</p>
 
-    Greeting from Mahesh Pawar.
-    Please find the attached document which contains Log of Deleted Dublicate file.
+    <p>Greeting from Mahesh Pawar.</p>
+    <p>Please find the attached document which contains Log of Deleted Dublicate file.</p>
     
-    Starting time of scanning: {stats['start_time']}
-    Total number of files scanned: {stats['files_scanned']}
-    Total number of duplicate files found: {stats['duplicates_found']} 
+    <B style = "color:black;">Starting time of scanning: {stats['start_time']}</B><br>
+    <B style = "color:black;">Total number of files scanned: {stats['files_scanned']}</B><br>
+    <B style = "color:black;">Total number of duplicate files found: {stats['duplicates_found']}</B><br>
 
-    NOTE : This is auto generated mail.
+    <p>NOTE : This is auto generated mail.<p>
 
-    Thanks and Regards,
+    <p>Thanks and Regards,</p>
 
-    Mahesh Dinkar Pawar
-    +91 9322150275"""
-        msg.attach(MIMEText(body, 'plain'))
+    </p>
+    Mahesh Dinkar Pawar<br>
+    RID : PM0100047<br>
+    +91 9322150275
+    </p>
+    </body>
+    </html>
+    """
+        msg.attach(MIMEText(body, 'html'))
 
         with open(log_file, "rb") as attachment:
             part = MIMEBase("application", "octet-stream")
@@ -80,6 +71,7 @@ def send_email(log_file, email_id, stats):
             server.starttls()
             server.login("try.web.new@gmail.com",  "qvbp xnrv pbcb pxaf")
             server.sendmail("try.web.new@gmail.com", email_id, msg.as_string())
+            print("Log file successfully sent to :",email_id)
     except Exception as e:
         print(f"Error sending email to {email_id}: {str(e)}")
 
@@ -95,13 +87,15 @@ def validate_interval(interval):
     if not interval.isdigit() or int(interval) <= 0:
         raise ValueError(f"Invalid interval: {interval}")
 
-def main_task(directory, email_id):
+
+def Main_Task(directory, email_id,interval):
+    
     start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     print(f"Scanning started at: {start_time}")
     try:
         all_files = [os.path.join(root, file) for root, _, files in os.walk(directory) for file in files]
-        duplicates = find_duplicates(directory)
-        delete_files(duplicates)
+        duplicates = Find_Duplicates(directory)
+        Delete_Files(duplicates)
         
         end_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         print(f"Scanning ended at: {end_time}")
@@ -115,44 +109,61 @@ def main_task(directory, email_id):
         
         print(f"Total files scanned: {stats['files_scanned']}")
         print(f"Total duplicate files found: {stats['duplicates_found']}")
+        ProcessLog()
+        log_file = "Marvellous/DuplicateFile.log"
+        Log_File(log_file, stats,duplicates)
+        Mail_Sender(log_file, email_id, stats)
 
-        log_file = "Marvellous/duplicate_removal_log.txt"
-        write_stats_to_file(log_file, stats)
-        send_email(log_file, email_id, stats)
     except Exception as e:
         print(f"Error in main_task: {str(e)}")
 
-def write_stats_to_file(file_path, stats):
+def Find_Duplicates(directory):
+    try:
+        checksums = {}
+        duplicates = []
+        for root, _, files in os.walk(directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                checksum = hashfile(file_path)
+                if checksum in checksums:
+                    duplicates.append(file_path)
+                else:
+                    checksums[checksum] = file_path
+        return duplicates
+    except Exception as e:
+        print(f"Error finding duplicates in {directory}: {str(e)}")
+        return []
+
+def Log_File(file_path, stats,dublicates = []):
     try:
         with open(file_path, 'w') as f:
-            f.write("Duplicate File Removal Operation\n")
-            f.write("---------------------------------\n")
+            separator = "_"*80
+
+            f.write(separator + "\n")
+            f.write("Welcome..! to Log File "+"\n")
+            f.write("Log File created at : "+time.ctime()+"\n")
+            f.write(separator + "\n")
+            f.write("Duplicate File Removal Operation : \n")
+            f.write(separator + "\n")
+
+            for element in dublicates: 
+                f.write("%s\n" % element)
+            f.write("="*80 + "\n")
             f.write(f"Starting time of scanning: {stats['start_time']}\n")
             f.write(f"Ending time of scanning: {stats['end_time']}\n")
             f.write(f"Total number of files scanned: {stats['files_scanned']}\n")
             f.write(f"Total number of duplicate files found: {stats['duplicates_found']}\n")
+           
     except Exception as e:
         print(f"Error writing stats to file: {str(e)}")
 
 def ProcessLog(log_dir="Marvellous"):
-    duplicates = []
     if not os.path.exists(log_dir):
         try:
             os.mkdir(log_dir)
         except:
             pass
 
-    separator = "_"*80
     log_path = os.path.join(log_dir, "DuplicateFile.log")
-    with open(log_path, "w") as f:
-        f.write(separator + "\n")
-        f.write("Welcome..! to Log File "+"\n")
-        f.write("Log File created at : "+time.ctime()+"\n")
-        f.write(separator + "\n")
-        f.write("Deleted Duplicate files are : " + "\n")
-        f.write(separator + "\n")
-
-        for element in duplicates:
-            f.write("%s\n" % element)
 
     print("Log File is successfully generated at location:", log_path)
